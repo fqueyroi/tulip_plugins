1283026e80740e34fe857556fa8024bf7b1f6934
Francois Queyroi <francois.queyroi@univ-nantes.fr>
2019-12-02 22:16:07 +0100

    fix Import Plugin

--- MetalArchivesScraping/MASimilarBandsCrawling.py
+++ MetalArchivesScraping/MASimilarBandsCrawling.py
@@ -1,4 +1,4 @@
-# When the plugin development is finished, you can copy the associated Python file 
+# You can copy the associated Python file 
 # to /PATH/TO/.Tulip-5.3/plugins/python
 # or /PATH/TO/TULIP/INSTALL/lib/tulip/python/
 # and it will be automatically loaded at Tulip startup
@@ -13,27 +13,26 @@ class MASimilarBandsCrawling(tlp.ImportModule):
     tlp.ImportModule.__init__(self, context)
     self.addStringParameter("url","Metal-archives url for the base band","https://www.metal-archives.com/bands/Weedeater/")
     self.addIntegerParameter("min score","Minimum score for bands to be considered similar","20")
-    self.addIntegerParameter("max depth","Maximum level of recursion for crawling (distance to base band)","1")
-    self.addStringParameter("logo folder path","Path for the folder in which the band logos are stored (leave blank to discard logos)","/home/queyroi-f/projects/metal_archive/scrapping/data/logos/")
-    self.base_url = "https://www.metal-archives.com/bands/Weedeater/"
+    self.addIntegerParameter("max depth","Maximum level of recursion for crawling (distance to base band)","2")
+    self.addStringParameter("logo folder path","Path for the folder in which the band logos are stored (leave blank to discard logos)","")
+    self.base_url = ""
     self.min_score = 20
-    self.max_depth = 1
-    self.logo_folder = "/home/queyroi-f/projects/metal_archive/scrapping/data/logos/"
+    self.max_depth = 2
+    self.logo_folder = ""
     
-  def addBand(self,band):
+  def addBand(self, band):
     id_     = self.graph.getStringProperty("id")
     name   = self.graph.getStringProperty("name")
-    size    = self.graph.getSizeProperty("viewSize")
     logo    = self.graph.getStringProperty("viewTexture")
-    color   = self.graph.getColorProperty("viewColor")
     
+    ## add other band info
     n = self.graph.addNode()
     id_[n]   = band.ma_id
     name[n] = band.name
     logo[n]    = band.logo_file_path
     return n   
   
-  def crawlSimilarBands(self,bandNodes,url,current_level):
+  def crawlSimilarBands(self, bandNodes, url, current_level):
     if current_level > self.max_depth:
       return
     print "Scrapping url: {}".format(url)
@@ -46,8 +45,7 @@ class MASimilarBandsCrawling(tlp.ImportModule):
     bandNodes[band.ma_id] = (band, n)
     
     for id_sim, url_sim, score_sim in band.similar_bands:
-#      id_sim, url_sim, score_sim = info_sim_bands
-      if score_sim >= self.min_score:
+      if score_sim >= self.min_score and id_sim not in bandNodes.keys():
         self.crawlSimilarBands(bandNodes,url_sim,current_level + 1) 
   
   
